9ff650a5753a1bb3b7f7f11b7c07b0ad7af4fcf1
Samantha Blum <samanthablum@Samanthas-MacBook-Pro.local>
2021-03-19 17:29:30 -0400

    added js for board array, win combos and set interval

--- main.js
+++ main.js
@@ -1,7 +1,39 @@
 // console.log('hello')
-const winningCombos = [
-    [4]
-    [4, 1]
-    [4, 1, 2]
-    [4, 1, 2, 4]
+let board;
+let btnId = null
+
+
+document.getElementById('start-button').addEventListener('click', () => {
+    // console.log('start the game!')
+});
+
+
+function getBoardArray () {
+    const board = [
+    1, 2,
+    3, 4,
+];
+// console.log(board.indexOf(1));
+}
+
+const winArrays = [
+    [3],
+    [3, 4],
+    [3, 4, 1],
 ]
+
+function winSequence (){
+    for(let i = 0; i < winArrays.length; i++) {
+        let sequence = winArrays[i]
+        for(let num of sequence) {
+          let elem = document.querySelector('#btn' + num)
+          elem.classList.remove('hide')  
+        }
+        
+    }
+
+}
+
+btnId = setInterval(winSequence, 1000)
+
+
