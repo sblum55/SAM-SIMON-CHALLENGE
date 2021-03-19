21089d3428ee2bd39570867918e9b6c26729cd91
Samantha Blum <samanthablum@Samanthas-MacBook-Pro.local>
2021-03-19 17:55:15 -0400

    add css styling to sequence buttons and added js for start game button

--- main.js
+++ main.js
@@ -3,8 +3,17 @@ let board;
 let btnId = null
 
 
-document.getElementById('start-button').addEventListener('click', () => {
+document.getElementById('start-button').addEventListener('click', function winSequence() {
     // console.log('start the game!')
+    for(let i = 0; i < winArrays.length; i++) {
+        let sequence = winArrays[i]
+        for(let num of sequence) {
+          let elem = document.querySelector('#btn' + num)
+          elem.classList.remove('hide')  
+        }
+        
+    }
+    
 });
 
 
@@ -21,18 +30,6 @@ const winArrays = [
     [3, 4],
     [3, 4, 1],
 ]
-
-function winSequence (){
-    for(let i = 0; i < winArrays.length; i++) {
-        let sequence = winArrays[i]
-        for(let num of sequence) {
-          let elem = document.querySelector('#btn' + num)
-          elem.classList.remove('hide')  
-        }
-        
-    }
-
-}
 
 btnId = setInterval(winSequence, 1000)
 
