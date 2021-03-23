8dae23fe0542a525a4fc4e337e6b1e31c0cd9d08
Samantha Blum <samanthablum@Samanthas-MacBook-Pro.local>
2021-03-22 21:48:06 -0400

    got game to compare answers and alert to win or loss of round

--- main.js
+++ main.js
@@ -12,36 +12,75 @@ let interval = 0
 let playerScore = 0
 let playerChoices;
 
-
-document.querySelector('#sequence-button').addEventListener('click', () => {
-    let btn = ``
-    computerChoice.length = 0
-    for (let i = 0; i <= 3; i++) {
-        let randNum = Math.floor(1 + (Math.random() * 4))
-        computerChoice.push(randNum)
-        btn = `btn${computerChoice[i]}`
-        showSequence(btn, interval)
-        interval += 750
-    } 
-    console.log(computerChoice)
-})
-
-function showSequence(btn, interval) {
-    setTimeout(() => {
-        document.getElementById(btn).classList.remove('hide')
-    }, interval)
-    setTimeout (() => {
-        for (let i = 0; i < colorBtns.length; i++) {
-            colorBtns[i].classList.add('hide')
-        }
-    },interval +500)
+//Create function round Start
+function computerTurn () {
+    document.querySelector('#sequence-button').addEventListener('click', () => {
+        let btn = ``
+        computerChoice.length = 0
+        for (let i = 0; i <= 3; i++) {
+            console.log('line 19 ran')
+            let randNum = Math.floor(1 + (Math.random() * 4))
+            computerChoice.push(randNum)
+            btn = `btn${computerChoice[i]}`
+            showSequence(btn, interval)
+            interval += 750
+        } 
+        console.log(computerChoice)
+    })
+    
+    function showSequence(btn, interval) {
+        setTimeout(() => {
+            document.getElementById(btn).classList.remove('hide')
+        }, interval)
+        setTimeout (() => {
+            for (let i = 0; i < colorBtns.length; i++) {
+                console.log('line 35 ran')
+                colorBtns[i].classList.add('hide')
+            }
+        },interval +500)
+    }
 }
 
-// playerChoices = () => {
-        for(let button of board) {
-            // console.log(button)
-            button.addEventListener('click', (event) => {
-                if(playerGuess.length < 4 && computerChoice.length === 4) {
+
+let compareAnswers = () => {
+        let isMatch = false
+        console.log(isMatch)
+        console.log(playerGuess.length === computerChoice.length)
+        let anouncement = document.querySelector('#resultText')
+        if(playerGuess.length === computerChoice.length) {
+
+        console.log(playerGuess, computerChoice)
+        // console.log(playerGuess === computerChoice)
+        
+        for(j = 0; j < playerGuess.length; j++) {
+            if (playerGuess[j] === computerChoice[j]) {
+                isMatch = true
+            } else {
+                return anouncement.innerText = 'You lost...game over.'
+            }
+        
+        }
+        console.log(isMatch)
+        if(isMatch === true ) {
+            
+                    anouncement.innerText = 'You won the round! Can you beat the next?'
+                    console.log('win')
+                    playerScore += 10
+                }
+                if(isMatch === false) {
+                    console.log('you made it here2')
+                    let anouncement = document.querySelector('#resultText')
+                    
+                    console.log('lost')
+                }
+        }        
+}
+
+playerChoices = () => {
+        for(let i = 0; i < board.length; i++) {
+            console.log('line 43 ran')
+            board[i].addEventListener('click', (event) => {
+                if(playerGuess.length < 4) {
                     const btn = event.target.firstChild.nextSibling
                     if(btn.classList.contains('hide')) {
                         btn.classList.remove('hide')
@@ -50,38 +89,18 @@ function showSequence(btn, interval) {
                     let btnContent = parseInt(btn.innerText)
                     playerGuess.push(btnContent)
                     showAnswers(colorBtns, interval)
+                    console.log(playerGuess)
+                    compareAnswers();
                     
-                }else if (playerGuess.length === 4 && computerChoice.length === 4) {
-                    const btn = event.target.firstChild.nextSibling
-                    if(btn.classList.contains('hide')) {
-                        btn.classList.remove('hide')
-                    }
-                    console.log(btn.innerText)
-                    let btnContent = parseInt(btn.innerText)
-                    playerGuess.push(btnContent)
-                    for(let j = 0; j < playerGuess.length; j++){
-                        console.log('you made it here')
-                        if(playerGuess[j] !== computerChoice[j]) {
-                            let anouncement = document.querySelector('#resultText')
-                            anouncement.innerText = 'You lost...game over.'
-                        }else {
-                            console.log('you made it here')
-                            let anouncement = document.querySelector('#resultText')
-                            anouncement.innerText = 'You won the round! Can you beat the next?'
-                        }
-                    }
-                    console.log(`It's not time to guess`)
                 }
             })
             
         }
-    console.log(playerGuess) 
-
-// };
-
-function gamePlay() {
     
-}
+};
+
+playerChoices();
+
 
 function showAnswers(colorBtns) {
     setTimeout (() => {
@@ -90,6 +109,19 @@ function showAnswers(colorBtns) {
         }
     }, 250)
 }
+
+document.querySelector('#player-score').innerText = playerScore
+
+function playerWin() {
+    if(playerScore === 100)
+    alert('You won!!')
+}
+
+// function gamePlay() {
+    
+// }
+
+
 
 // function gamePlay() {
 //     if(playerGuess == computerChoice) {
